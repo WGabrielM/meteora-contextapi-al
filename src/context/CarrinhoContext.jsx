5fed16f81dfed66c0937fbe52edfd2bab853885e
Wellington <wellingtongmb@gmail.com>
2024-09-05 16:11:10 -0300

    using reduce to create a sum of all itens on carrinho

--- src/context/CarrinhoContext.jsx
+++ src/context/CarrinhoContext.jsx
@@ -1,14 +1,26 @@
 import { createContext, useState } from "react";
 
-export const CarrinhoContext= createContext();
+export const CarrinhoContext = createContext();
 
-CarrinhoContext.displayName = "Carrinho"
+CarrinhoContext.displayName = "Carrinho";
 
-export const CarrrinhoProvider = ({children}) => {
-    const [carrinho, setCarrinho] = useState([]);
-    return (
-        <CarrinhoContext.Provider value={{carrinho, setCarrinho}}>
-            {children}
-        </CarrinhoContext.Provider>
-    )
-}
+export const CarrrinhoProvider = ({ children }) => {
+  const [carrinho, setCarrinho] = useState([]);
+  const [quantidade, setQuantidade] = useState(0);
+  const [valorTotal, setValorTotal] = useState(0);
+
+  return (
+    <CarrinhoContext.Provider
+      value={{
+        carrinho,
+        setCarrinho,
+        quantidade,
+        setQuantidade,
+        valorTotal,
+        setValorTotal,
+      }}
+    >
+      {children}
+    </CarrinhoContext.Provider>
+  );
+};
